7f0bf633ea53c49e81b95e5a2d2d4d75067a67ec
Krishna <krishna9336@gmail.com>
2023-09-12 22:20:53 +0530

    initial commit

---
+++ data/database.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const connectDB=()=>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>
+    console.log("Database is Connected...")).catch((e)=>console.log(e));
+}
---
+++ models/user.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        unique : true,
+        required:true,
+    },
+    password:{
+        type:String,
+        select:false,
+    },
+    createdAt:{
+        type:Date,
+        default:new Date(),
+    }
+});
+
+export const User = mongoose.model("User",schema);
